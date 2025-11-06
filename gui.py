175e5d7cc117d5305dcdc5d8303e9408b494f86b
= <=>
2025-11-05 19:51:26 -0800

    Looked into a gui application for this to do list.

---
+++ gui.py
@@ -0,0 +1,13 @@
+from toDoList import ToDoList
+import tkinter as tk
+from tkinter import ttk
+
+
+guiLogic = ToDoList()
+root = tk.Tk()
+root.title("To Do List")
+
+#message = tk.Label(root, text = "To Do List")
+#message.pack()
+
+root.mainloop()
