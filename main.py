99539006020d431216f6257c1410558144da2404
= <=>
2025-12-07 15:31:17 -0800

    New version with basic database usage.

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
-class TaskTracker:
-    def add_task(self, task: str):
-        pass
-    def remove_task(self, task: str):
-        pass
-    def list_tasks(self):
-        pass
+# NOTE: imports sqlite3, datetime, os
+import os
+from datetime import date, datetime
+import argparse
+import sqlite3
+from platform import system
+from typing import Union
 
-if __name__ == "__main__":
-    try:
-        import sqlite3
-    except:
-        print("Failed to import sqlite3.")
-        exit()
+# NOTE: declare constants
+DATE_TODAY = date.today().isoformat()
 
+# NOTE: Create DB & Create Table
+if not os.path.isfile("tasks.db"):
+    connection = sqlite3.connect("tasks.db")
+    cursor = connection.cursor()
+    cursor.executescript("""
+        BEGIN;
+        CREATE TABLE taskTable(task TEXT, due_date DATE, complete BOOLEAN);
+        COMMIT;
+        """)
+else:
     connection = sqlite3.connect("tasks.db")
     cursor = connection.cursor()
 
-    test = cursor.execute("""
-        SELECT name FROM sqlite_master WHERE type='table'
-        AND name='taskTable'; """).fetchall()
-    if len(test) == 0:
-        cursor.execute("CREATE TABLE taskTable (task, date, complete)")
-        cursor.execute("""
-            INSERT INTO taskTable VALUES
-                ('dishes', 2025-12-6, false),
-                ('laundry', 2025-12-8, false)
-        """)
-
-    for result in cursor.execute("SELECT task, date, complete FROM taskTable"):
-        print(result)
-
+# NOTE: Functions to list, add, complete tasks
+def add_task(task: str, date: datetime = DATE_TODAY) -> None:
+    cursor.execute(f"INSERT INTO taskTable (task, due_date, complete) VALUES('{task}', '{date}', {int(False)})")
     connection.commit()
-    connection.close()
+def complete_task(task: str) -> None:
+    cursor.execute(f"UPDATE taskTable SET complete = {int(True)} WHERE task = '{task}'")
+    connection.commit()
+def list_task(date: Union[datetime, str] = DATE_TODAY, complete: bool = False) -> None:
+    for result in cursor.execute(f"SELECT * FROM taskTable WHERE due_date <= '{date}' AND complete = {int(complete)}"):
+        print(result)
+# NOTE: Read args
+
+
+# NOTE: Start testing here.
+add_task("Dishes")
+complete_task("Dishes")
+list_task(complete=True)
+
+# End the program
+cursor.close()
