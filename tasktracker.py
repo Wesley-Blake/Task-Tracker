818681cbc189bf5311f7dbdcb370b44b80959b28
wesblake@proton.me <wesblake@proton.me>
2025-12-08 12:45:45 -0800

    Added notes to my self.

--- tasktracker.py
+++ tasktracker.py
@@ -8,6 +8,7 @@ from datetime import date, datetime
 
 class TaskTracker:
     def __init__(self, database: str, table: str):
+        # NOTE: Validate input strings.
         self.database = database
         self.table = table
         self.connection = sqlite3.connect(self.database)
@@ -26,11 +27,14 @@ class TaskTracker:
         """)
         return True
 
+    # NOTE: Add date quiry, union?
     def list_task(self, complete: bool = False) -> list: # return list of tuples
+        # NOTE: Validate input
         list_task_sql = f"SELECT * FROM '{self.table}' WHERE complete={int(complete)}"
         return self.cursor.execute(list_task_sql).fetchall()
     
     def add(self, task: str, due_date: datetime = date.today()) -> None:
+        # NOTE: Validate input
         add_sql = f"INSERT OR IGNORE INTO {self.table} VALUES('{task}','{due_date}',{int(False)})"
         self.cursor.execute(add_sql)
         self.connection.commit()
@@ -40,11 +44,13 @@ class TaskTracker:
                 due_date: datetime = date.today(),
                 complete: bool = True
                 ) -> None:
+        # NOTE: Validate input
         complete_sql = f"UPDATE {self.table}\
                         SET complete={int(complete)}\
                         WHERE task_name='{task}' AND due_date='{due_date}'"
         self.cursor.execute(complete_sql)
         self.connection.commit()
+    # NOTE: Commit function?
     def close():
         self.connection.commit()
         self.connection.close()
